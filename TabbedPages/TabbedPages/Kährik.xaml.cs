c7019ed0283c06d92ce2a4d64fc930235baa9498
Kristina Pankova <kristinapankova@outlook.com>
2020-10-16 02:14:42 +0300

    Добавьте файлы проекта.

---
+++ TabbedPages/TabbedPages/Kährik.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Kährik : TabbedPage
+    {
+        public Kährik()
+        {
+            InitializeComponent();
+        }
+    }
+}
